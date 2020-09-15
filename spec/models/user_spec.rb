52fe58ba6a14605021877247e2d490171c1e736a
Daikichi1223 <abcdaiki1223@icloud.com>
2020-09-15 20:33:27 +0900

    RSpecの導入

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+describe User do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまく行く時' do
+      
+    end
+  end
+end
