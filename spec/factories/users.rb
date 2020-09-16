b16699eb5178bcbd6036e008b649f278998ec6b5
Daikichi1223 <abcdaiki1223@icloud.com>
2020-09-16 14:24:53 +0900

    FactoryBotの導入

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"abe"}
+    email                 {"kkk@gmail.com"}
+    password              {"00000000"}
+    last_name             {"ふりま"}
+    first_name            {"太郎"}
+    last_name_ruby        {"フリマ"}
+    first_name_ruby       {"タロウ"}
+    birthday              {"1930-01-01"}
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,52 @@ describe User do
   end
 
   describe 'ユーザー新規登録' do
-    context '新規登録がうまく行く時' do
-      
+    context '新規登録がうまくいく時' do
+      it "nicknameとemail、password、last_name、first_name、last_name_ruby、first_name_rubyとbirthdayが存在すれば登録できる" do
+      end
+      it "passwordが6文字以上であれば登録できる" do
+      end
+      it "passwordが半角英数字混合であれば登録できる" do
+      end
+      it "last_nameとfirst_nameが全角であれば登録できる" do
+      end
+      it "last_name_rubyとfirst_name_rubyが全角カタカナであれば登録できる" do
+      end
+    end
+
+    context '新規登録がうまくいかない時' do
+      it "nicknameが空だと登録できない" do
+      end
+      it "emailが空だと登録できない" do
+      end
+      it "重複したemailが存在する場合は登録できない" do
+      end
+      it "passwordが空だと登録できない" do
+      end
+      it "passwordが6文字以下だと登録できない" do
+      end
+      it "passwordが半角英数字でなければ登録できない" do
+      end
+      it "passwordは2回入力しないと登録できない" do
+      end
+      it "last_nameが空だと登録できない" do
+      end
+      it "last_nameが全角でないと登録できない" do
+      end
+      it "first_nameが空だと登録できない" do
+      end
+      it "first_nameが全角でないと登録できない" do
+      end
+      it "last_name_rubyが空だと登録できない" do
+      end
+      it "last_name_rubyが全角カタカナでないと登録できない" do
+      end
+      it "first_name_rubyが空だと登録できない" do
+      end
+      it "first_name_rubyが全角カタカナでないと登録できない" do
+      end
+      it "birthdayが空だと登録できない" do
+      end
     end
   end
 end
