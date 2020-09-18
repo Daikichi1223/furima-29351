ac8981aed678f3a9b506e2e2c23d42006813b88c
Daikichi1223 <abcdaiki1223@icloud.com>
2020-09-18 11:50:49 +0900

    入力必須条件のバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,10 +5,20 @@ class Item < ApplicationRecord
   has_one_attached :image
   
   belongs_to_active_hash :category
+
+  with_options presence: true do
+    validates :image, presence: { message: 'を選択してください'}
+    validates :name
+    validates :explanation
+    validates :category_id, presence: { message: 'を選択してください'}
+    validates :condition_id, presence: { message: 'を選択してください'}
+    validates :delivary_fee_id, presence: { message: 'を選択してください'}
+    validates :exhibitor_area_id, presence: { message: 'を選択してください'}
+    validates :delivary_day_id, presence: { message: 'を選択してください'}
+    validates :price
+  end
+
   
-  validates :category_id, numericality: { other_than: 1 }
-  validates :condition_id, numericality: { other_than: 1 }
-  validates :delivary_fee_id, numericality: { other_than: 1 }
-  validates :exhibitor_area_id, numericality: { other_than: 1 }
-  validates :delivary_day_id, numericality: { other_than: 1 }
+
+
 end
