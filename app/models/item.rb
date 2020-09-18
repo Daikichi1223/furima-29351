5c7a83d23635e70947f6497970647b1274277e39
Daikichi1223 <abcdaiki1223@icloud.com>
2020-09-18 12:01:43 +0900

    出品価格のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,18 +7,18 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
 
   with_options presence: true do
-    validates :image, presence: { message: 'を選択してください'}
+    validates :image, presence: { message: "を選択してください"}
     validates :name
     validates :explanation
-    validates :category_id, presence: { message: 'を選択してください'}
-    validates :condition_id, presence: { message: 'を選択してください'}
-    validates :delivary_fee_id, presence: { message: 'を選択してください'}
-    validates :exhibitor_area_id, presence: { message: 'を選択してください'}
-    validates :delivary_day_id, presence: { message: 'を選択してください'}
+    validates :category_id, presence: { message: "を選択してください"}
+    validates :condition_id, presence: { message: "を選択してください"}
+    validates :delivary_fee_id, presence: { message: "を選択してください"}
+    validates :exhibitor_area_id, presence: { message: "を選択してください"}
+    validates :delivary_day_id, presence: { message: "を選択してください"}
     validates :price
   end
 
-  
+  validates :price, format: {with: /\A[0-9]+\z/, message: "は半角数字で入力してください"}
 
 
 end
