2271bdcdc6061b59b92d46b1ad425218e18115bd
Daikichi1223 <abcdaiki1223@icloud.com>
2020-09-25 08:10:56 +0900

    ページ遷移の制御の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,12 @@
 class OrdersController < ApplicationController
+before_action :authenticate_user!
+  def index
+    unless user_signed_in? && current_user.id == params[:item_id]
+      redirect_to root_path
+    end
+    @item = Item.find(params[:item_id])
+  end
+
   def create
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :users
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
-  resources :orders, only:[:create]
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :orders, only: [:index, :create]
+  end
 end
