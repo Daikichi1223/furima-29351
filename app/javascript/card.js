15d9ea22042a83916d9fbf824081340b58f76e5b
Daikichi1223 <abcdaiki1223@icloud.com>
2020-09-25 13:57:55 +0900

    クレカ情報の送信とトークンの発行

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -12,6 +12,11 @@ const pay = ()=> {
       exp_month: formData.get("exp_month"),
       exp_year: `20${formData.get("exp_year")}`,
     };
+
+    Payjp.createToken(card, (status, response) => {
+      console.log(status)
+      console.log(response)
+    })
   })
 }
 
