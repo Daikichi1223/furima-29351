9dc811a40b42a0e692472b91a978c44cdd33291c
Daikichi1223 <abcdaiki1223@icloud.com>
2020-09-25 13:24:22 +0900

    JSでブラウザのカード情報取得の実装

---
+++ app/javascript/card.js
@@ -0,0 +1,18 @@
+const pay = ()=> {
+  Payjp.setPublicKey("pk_test_570137d47b8cfee386fe574f");
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+    const card = {
+      number: formData.get("number"),
+      cvc: formData.get("cvc"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+    };
+  })
+}
+
+window.addEventListener("load", pay);
